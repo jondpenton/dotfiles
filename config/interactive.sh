eb78709d7632634a6790016bd0816e168f2928f8
Jon Penton <hey@jondpenton.com>
2023-09-20 19:12:52 -0400

    Initial commit

---
+++ config/interactive.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# use inline `export VAR=...` statements, for fish compatibility`:
+# shellcheck disable=SC2034
+
+# load the default configuration
+. "$DOROTHY/config/interactive.sh"
+
+export DOROTHY_THEME='starship'
