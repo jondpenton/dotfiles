c61d34ef63e7bbcbcbc5bf38388601d6cc592610
Jon Penton <hey@jondpenton.com>
2023-09-20 20:41:37 -0400

    feat: :sparkles: setup configs

---
+++ config/git.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# do not use `export` keyword in this file:
+# shellcheck disable=SC2034
+
+# Used by `setup-git`
+# You can use `setup-git --configure` to configure these.
+
+# GPG_SIGNING_KEY='' # use 'krypton' for Krypt.co
+GIT_DEFAULT_BRANCH='main'
+GIT_PROTOCOL='ssh' # 'https', or 'ssh'
+GIT_NAME='Jon Penton'
+GIT_EMAIL='hey@jondpenton.com'
+# MERGE_TOOL=''
+GITHUB_USERNAME='jondpenton'
+# GITLAB_USERNAME=''
