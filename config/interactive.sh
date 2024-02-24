cbd7d2edcc0f0003bf242637e61adbb89f8e808e
Jon Penton <hey@jondpenton.com>
2024-02-23 23:16:43 -0500

    reset repo

--- config/interactive.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# use inline `export VAR=...` statements, for fish compatibility`:
-# shellcheck disable=SC2034
-
-# load the default configuration
-. "$DOROTHY/config/interactive.sh"
-
-export DOROTHY_THEME='starship'
