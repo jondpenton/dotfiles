5afaccb1704d66dd5415e116bd140d5466fbd143
Jon Penton <hey@jondpenton.com>
2024-02-24 11:56:37 -0500

    setup initial utils

---
+++ config/git.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# do not use `export` keyword in this file:
+# shellcheck disable=SC2034
+
+# load the default configuration
+source "$DOROTHY/config/git.bash"
+
+# Used by `setup-git`
+# You can use `setup-git --configure` to configure these.
+
+GPG_SIGNING_KEY=''
+GIT_DEFAULT_BRANCH='main'
+GIT_PROTOCOL='ssh'
+GIT_NAME='Jon Penton'
+GIT_EMAIL='hey@jondpenton.com'
+MERGE_TOOL='delta'
+GITHUB_USERNAME='jondpenton'
+GITLAB_USERNAME=''
---
+++ config/interactive.nu
@@ -0,0 +1,5 @@
+#!/usr/bin/env nu
+
+# load the dorothy defaults
+source ~/.local/share/dorothy/config/interactive.nu
+$env.DOROTHY_THEME = 'starship'
